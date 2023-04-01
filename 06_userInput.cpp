3bfa587109613e2e264835f79029917d8b62a1f6
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-04-01 19:48:37 +0530

    New update

---
+++ 06_userInput.cpp
@@ -0,0 +1,12 @@
+/* Collect user input and display it simply */
+
+#include<iostream>
+using namespace std; 
+//code by krish
+int main(){ 
+     int  a; 
+     cout<<"Enter a number: ";  
+     cin>>a;  
+     cout<<"Given number is = "<<a; 
+  return 0; 
+} 
