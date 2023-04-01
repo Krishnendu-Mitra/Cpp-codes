4410ce4268915739e00cd95adb115a007c4caa76
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-04-01 19:47:40 +0530

    New update

---
+++ 05_power.cpp
@@ -0,0 +1,13 @@
+/* Print the power of a number like the number 
+is 5 and power is 5^2 = 25 or 3 and 3^2 = 9 etc */
+
+#include<iostream>
+using namespace std;
+//code by krish
+int main(){ 
+    int  a, p;  
+    a = 10;  
+    p = a * a;  
+    cout<<"Power in 2 is = "<<p; 
+  return 0; 
+} 
