c580bb4e503796e92b95ac3972f6fa264bc7b406
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-04-01 19:58:10 +0530

    New update

---
+++ 02_variableAssign.cpp
@@ -0,0 +1,12 @@
+/* Assign an integer variable interlay 
+and display it simply */
+
+#include<iostream>
+using namespace std; 
+//code by krish
+int main(){ 
+     int  a; 
+     a = 10;
+     cout<<a;
+  return 0; 
+} 
